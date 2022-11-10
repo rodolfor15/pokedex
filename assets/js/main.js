0ed8f2fef0f9a5585fbd83d56f96e60df4508c77
andrehugofernandes <andrehugofernandes@gmail.com>
2022-11-10 18:53:25 -0300

    AUMENTANDO O LIMITE PARA 10

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 let offset = 0
-const limit = 5
+const limit = 10
 const maxRecords = 151
 
 function convertPokemnonTypesToLi(pokemonTypes){
