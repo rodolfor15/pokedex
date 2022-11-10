bd63857f907abb507c66e044801f5e217c51a362
andrehugofernandes <andrehugofernandes@gmail.com>
2022-11-10 09:47:53 -0300

    criando o butão load more para paginação

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -25,7 +25,7 @@ pokeApi.getPokemonDetail = (pokemon) => {
 }
 
 
-pokeApi.getPokemons = (offset = 0, limit = 200) => {
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
   return fetch(url)
