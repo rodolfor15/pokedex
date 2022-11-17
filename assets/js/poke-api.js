c18ee59864977bc4cb54dd6000307b35e52d7f2d
andrehugofernandes <andrehugofernandes@gmail.com>
2022-11-17 09:54:19 -0300

    Finalizando para GITHUB

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -5,16 +5,12 @@ function convertPokeApiDetailToPokemon (pokeDetail){
   const pokemon = new Pokemon()
   pokemon.number = pokeDetail.id
   pokemon.name = pokeDetail.name
-
   const types = pokeDetail.types.map((typeSlot) =>  typeSlot.type.name)
-
   const [type] = types
-  
   pokemon.types = types
   pokemon.type = type
   pokemon.photo = pokeDetail.sprites.other.home.front_default
   // pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
-
   return pokemon
 
 }
